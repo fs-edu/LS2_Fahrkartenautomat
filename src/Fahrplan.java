185975f538e1a7fa6704e1740a39400ff2632156
fs-edu <felix.sardison@procurand.de>
2022-11-30 10:22:13 +0100

    Fertig

--- src/Fahrplan.java
+++ src/Fahrplan.java
@@ -7,6 +7,10 @@ public class Fahrplan {
         char haltInSpandau = 'n';
         char richtungHamburg = 'n';
         char haltInStendal = 'j';
+        char haltInWolfsburg = 'j';
+        char haltInBraunschweig = 'j';
+        String Msg = "Sie erreichen ";
+
         char endetIn = 'h';
 
         fahrzeit = fahrzeit + 8; // Fahrzeit Hbf -> Spandau
@@ -14,6 +18,30 @@ public class Fahrplan {
         if (haltInSpandau == 'j') {
             fahrzeit = fahrzeit + 2; // Halt in Spandau
         }
+        //
+        if (richtungHamburg == 'j') {
+            fahrzeit = fahrzeit + 96;
+            System.out.println(Msg + "Hamburg in " + fahrzeit);
+            return;
+        } else {
+            fahrzeit = fahrzeit + 34;
+        }
 
+        if (haltInStendal == 'j') {
+            fahrzeit = fahrzeit + 16;
+        } else {
+            fahrzeit = fahrzeit + 6;
+        }
+        //
+        if (haltInWolfsburg == 'j') {
+            fahrzeit = fahrzeit + 29;
+            System.out.println(Msg + "Wolfsburg in " + fahrzeit);
+        } else if (haltInBraunschweig == 'j') {
+            fahrzeit = fahrzeit + 50;
+            System.out.println(Msg + "Braunschweig in " + fahrzeit);
+        } else {
+            fahrzeit = fahrzeit + 62;
+            System.out.println(Msg + "Hannover in " + fahrzeit);
+        }
     }
 }
