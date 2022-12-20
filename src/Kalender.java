908ca0f409d7ea11993fea2347ee4d6e2fc9c09b
fs-edu <felix.sardison@procurand.de>
2022-12-20 14:30:08 +0100

    Kalenderdings vernbessert

--- src/Kalender.java
+++ src/Kalender.java
@@ -2,40 +2,41 @@ import java.util.Scanner;
 
 public class Kalender {
 
-    public static void main(String[] args) {
-        Scanner tastatur = new Scanner(System.in);
-        System.out.println("Hallo, dies ist die Kalender App");
+    public static int findMonth(int monat) {
 
-        Integer[] D31 = {1,3,5,7,9,12};  // "Januar", "März", "Mai", "Juli", "August", "Oktober", "Dezember"
-        Integer[] D30 = {4,6,10,11}; // "April", "Juni", "September", "November"
+        // "Januar", "März", "Mai", "Juli", "August", "Oktober", "Dezember"
+        Integer[] D31 = {1,3,5,7,9,12};
+        // "April", "Juni", "September", "November"
+        Integer[] D30 = {4,6,10,11};
 
         int tage = 0;
-        int monat;
-
-        System.out.print("Wie lautet der Monat (1-12): ");
-        monat = tastatur.nextInt();
-
-        if (monat == 2 ) {
-            tage = 28;
-            System.out.printf("Der %d-te Monat im Jahr hat %d Tage.", monat, tage);
-            return;
-        }
 
         for (Integer value : D31) {
             if (value == monat) {
                 tage = 31;
-                System.out.printf("Der %d-te Monat im Jahr hat %d Tage.", monat, tage);
-                return;
+                return tage;
             }
         }
 
         for (Integer integer : D30) {
             if (integer == monat) {
                 tage = 30;
-                System.out.printf("Der %d-te Monat im Jahr hat %d Tage.", monat, tage);
-                return;
+                return tage;
             }
         }
+
+        // wenn februar
+        return 28;
+    }
+
+    public static void main(String[] args) {
+        Scanner tastatur = new Scanner(System.in);
+        System.out.println("Hallo, dies ist die Kalender App");
+        System.out.print("Wie lautet der Monat (1-12): ");
+        int Monat = tastatur.nextInt();
+
+        Integer Tage = findMonth(Monat);
+        System.out.printf("Der %d-te Monat im Jahr hat %d Tage.", Monat, Tage);
 
         tastatur.close();
 
